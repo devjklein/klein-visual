94a5beb1e6c5e3331d50ec8f0b255882a169fd7b
devjklein <38089945+devjklein@users.noreply.github.com>
2018-06-19 17:05:46 -0400

    Major overhaul of the menu code
    Overhauled the menu code using new touch-based interaction detection and not browser-based. Also features keyboard navigation support.

--- js/access.js
+++ js/access.js
@@ -1,102 +1,248 @@
 /*------------------------------------*\
-  ACCESSIBILITY AND MENUS
+  DETECT-TOUCH-AND-HOVER
 \*------------------------------------*/
 
+//https://stampede-design.com/blog/dropdown-menu-hover/#disqus_thread
+//https://joshtronic.com/2015/04/19/handling-click-and-touch-events-on-the-same-element/
+//https://codeburst.io/the-only-way-to-detect-touch-with-javascript-7791a3346685
+
+// Initialize variables for interaction states
+var canHover = false;
+var canTouch = false;
+
+// Detect if touch is happening. If so, disable hover, but keep looking for hover events.
+function detectTouch() {
+  window.addEventListener("touchstart", function onFirstTouch() {
+    console.log("Can Touch!");
+    canTouch = true;
+    if (canHover == true) {
+      canHover = false;
+      hoverOff();
+      detectHover();
+    };
+    touchOn();
+    window.removeEventListener("touchstart", onFirstTouch, false);
+  }, false); 
+};
+
+// Detect if hover is happening. If so, disable touch, but keep looking for touch events.
+function detectHover() {
+  window.addEventListener("mouseover", function onFirstHover() {
+    console.log("Can Hover!");
+    canHover = true;
+    if (canTouch == true) {
+      canTouch = false;
+      touchOff();
+      detectTouch();
+    };
+    hoverOn();
+    window.removeEventListener("mouseover", onFirstHover, false);
+  }, false);
+};
+
+// Run both functions
+detectTouch();
+detectHover();
+
+
+/*------------------------------------*\
+  MAIN-FUNCTIONS
+\*------------------------------------*/
+
+// A timer for delayed close when hovering
 var timer;
-var isMobile = false;
-if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-   || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
-  isMobile = true;
-};
 
+var dropdownMenu = ".nav-dropdown";
+var dropdownButton = ".nav-dropdown > a";
+var dropdownList = ".nav-dropdown > ul";
+var navButton = ".main-nav-btn";
+var navList = ".main-nav"
+var navIcon = "#main-nav-btn__icon"
 
-
-
-
-
-//https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device-in-jquery
-if (isMobile == true) { // If on mobile, for each dropdown "a" link wait for touchstart
-  $(".nav-dropdown > a").each(function(i, el){
-
-
-    el.addEventListener("touchstart", function(event){
-      if ($(".nav-dropdown > a").attr('aria-expanded') == "true") { 
-        $(".nav-dropdown > a").attr('aria-expanded', "false");
-        $(".nav-dropdown ul").slideUp("fast"); // Hard close
-      } else {
-        $(".nav-dropdown > a").attr('aria-expanded', "true");
-        $(".nav-dropdown ul").slideDown("fast"); // Hard open
-        clearTimeout(timer);
-      };
-    });
+// Close when interacting outside menu
+function htmlClose(clickType, ariaObj, closeObj) {
+  $("html")[0].addEventListener(clickType, function(event){
+    if ($(ariaObj).attr("aria-expanded") == "true") { 
+      closeObj();
+    };
   });
   
-  
-} else { // If on desktop, use hover and click
-  $(".nav-dropdown").each(function(i, el){
-    
-    
-    el.addEventListener("mouseover", function(event){
-      $(".nav-dropdown ul").slideDown("fast"); // While mouseover, hard open
-      $(".nav-dropdown > a").attr('aria-expanded', "true");
-      clearTimeout(timer);
-    });
-
-
-    el.addEventListener("mouseout", function(event){ // While mouseout, hard close
-      timer = setTimeout(function(event){
-        $(".nav-dropdown ul").slideUp("fast");
-        $(".nav-dropdown > a").attr('aria-expanded', "false");
-      }, 1000);
-    });
-
-
-    $(".nav-dropdown > a").each(function(i, el){
-      el.addEventListener("click", function(event){
-        $(".nav-dropdown ul").slideToggle("fast"); // Toggle on click
-        if ($(".nav-dropdown > a").attr('aria-expanded') == "true") {
-          $(".nav-dropdown > a").attr('aria-expanded', "false");
-        } else {
-          $(".nav-dropdown > a").attr('aria-expanded', "true");
-        };
-        event.preventDefault();
-      });
-    });
+  // When using menu, do not propagate interaction to HTML
+  $(ariaObj)[0].addEventListener(clickType, function(event){
+    if ($(ariaObj).attr("aria-expanded") == "true") { 
+      event.stopPropagation();
+    };
   });
-};
-
-
-
-
-
-$(".main-nav-btn").each(function(i, el){
-  if (isMobile == true) { // If on mobile, use touch
-    el.addEventListener("touchstart", function(event){
-      if ($(".main-nav-btn").attr('aria-expanded') == "true") {
-        $(".main-nav").slideUp("fast");
-        $(".main-nav-btn").attr('aria-expanded', "false");
-        $(".main-nav-btn").removeClass("open");
-        $('#main-nav-btn__icon').attr("class", "");
-      } else {
-        $(".main-nav").slideDown("fast");
-        $(".main-nav-btn").attr('aria-expanded', "true");
-        $(".main-nav-btn").addClass("open");
-        $('#main-nav-btn__icon').attr("class", "open");
-      };
-    });
-  } else { // If on desktop, use click
-    el.addEventListener("click", function(event){
-      if ($(".main-nav-btn").attr('aria-expanded') == "true") {
-        $(".main-nav").slideUp("fast");
-        $(".main-nav-btn").attr('aria-expanded', "false");
-        $(".main-nav-btn").removeClass("open");
-        $('#main-nav-btn__icon').attr("class", "");
-      } else {
-        $(".main-nav").slideDown("fast");
-        $(".main-nav-btn").attr('aria-expanded', "true");
-        $(".main-nav-btn").addClass("open");
-        $('#main-nav-btn__icon').attr("class", "open");
+  
+  // Don't close the main menu when clicking on the dropdown
+  if (ariaObj == navButton) {
+    $(navList)[0].addEventListener(clickType, function(event){
+      if ($(navButton).attr("aria-expanded") == "true") { 
+        event.stopPropagation();
       };
     });
   };
+};
+
+
+
+
+
+/*------------------------------------*\
+  TOUCH
+\*------------------------------------*/
+
+// Turn on touch by listening for touchstart events
+function touchOn() {
+  $(dropdownButton).each(function(i, el){
+    el.addEventListener("touchstart", dropdownToggle);
+    htmlClose("touchstart", dropdownButton, dropdownClose);
+  });
+  
+  $(navButton).each(function(i, el){
+    el.addEventListener("touchstart", dropdownToggle);
+   htmlClose("touchstart", navButton, menuClose);
+  });
+};
+
+// Turn off touch by removing touchstart events
+function touchOff() {
+  $(dropdownButton)[0].removeEventListener("touchstart", dropdownToggle);
+  $(navButton)[0].removeEventListener("touchstart", dropdownToggle);
+};
+
+
+
+
+
+/*------------------------------------*\
+  HOVER
+\*------------------------------------*/
+
+// Turn on hover by listening for mouseover and click events
+function hoverOn() {
+  $(dropdownMenu).each(function(i, el){
+    el.addEventListener("mouseover", dropHoverStart);
+    el.addEventListener("mouseout", dropHoverEnd);
+    htmlClose("click", dropdownMenu, dropdownClose);
+  });
+  
+  $(navButton).each(function(i, el){
+    el.addEventListener("click", navigationToggle);
+    htmlClose("click", navButton, menuClose);
+  });  
+};
+
+
+function dropHoverStart(event){
+  dropdownOpen();
+};
+
+function dropHoverEnd(event){ // While mouseout, hard close
+  timer = setTimeout(function(event){
+    dropdownClose();
+  }, 1000);
+};
+
+function navigationToggle(event){
+  if ($(navButton).attr("aria-expanded") == "true") {
+    menuClose();
+  } else {
+    menuOpen();
+  };
+};
+
+function dropdownToggle(event){
+  if ($(dropdownButton).attr("aria-expanded") == "true") {
+    dropdownClose();
+  } else {
+    dropdownOpen();
+  };
+  // So the a link isn't followed
+  event.preventDefault();
+};
+
+// Turn off hover by removing mouseover and click events
+function hoverOff() {
+  $(dropdownMenu)[0].removeEventListener("mouseover", dropHoverStart);
+  $(dropdownMenu)[0].removeEventListener("mouseout", dropHoverEnd);
+  $(navButton)[0].removeEventListener("click", navigationToggle);
+  $(dropdownButton)[0].removeEventListener("click", dropdownToggle);
+};
+
+
+
+
+
+/*------------------------------------*\
+  KEYBOARD
+\*------------------------------------*/
+$(dropdownButton).each(function(i, el){
+  el.addEventListener("keypress", dropdownKeyboardOpen);
 });
+
+$(navButton).each(function(i, el){
+  el.addEventListener("keypress", navigationKeyboardOpen);
+});
+
+
+function dropdownKeyboardOpen(event) {
+  var key = event.which || event.keyCode;
+  if (key === 13 || key === 32) { // 13 is enter, 32 is space
+    dropdownOpen();
+  } else if (key === 27) { // 27 is Escape
+    dropdownClose();
+  };
+};
+
+function navigationKeyboardOpen(event) {
+  var key = event.which || event.keyCode;
+  if (key === 13 || key === 32) { // 13 is enter, 32 is space
+    menuOpen();
+    event.preventDefault();
+  } else if (key === 27) { // 27 is Escape
+    menuClose();
+    event.preventDefault();
+  };
+};
+
+
+
+
+
+/*------------------------------------*\
+  DROPDOWN-FUNCTIONS
+\*------------------------------------*/
+
+function dropdownOpen() {
+  $(dropdownButton).attr("aria-expanded", "true");
+  $(dropdownList).slideDown("fast"); // Hard open
+  clearTimeout(timer);
+};
+
+function dropdownClose() {
+  $(dropdownButton).attr("aria-expanded", "false");
+  $(dropdownList).slideUp("fast"); // Hard close
+};
+
+
+
+
+
+/*------------------------------------*\
+  MAIN-MENU-FUNCTIONS
+\*------------------------------------*/
+
+function menuOpen() {
+  $(navList).slideDown("fast");
+  $(navButton).attr("aria-expanded", "true");
+  $(navButton).addClass("open");
+  $(navIcon).attr("class", "open");
+};
+
+function menuClose() {
+  $(navList).slideUp("fast");
+  $(navButton).attr("aria-expanded", "false");
+  $(navButton).removeClass("open");
+  $(navIcon).attr("class", "");
+};
