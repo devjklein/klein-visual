405f7270d1a82662845ecde34823141dc3afa1a7
devjklein <38089945+devjklein@users.noreply.github.com>
2018-06-19 22:05:27 -0400

    Condensed and reverted menu backend
    Condensed my menu jQuery back to much of it's original form, only retaining the function optimizations that I gathered along the way. For now, the menu operates solely with click functions, and in testing it worked just fine. It may stay the way it is, as without touch events it works on any device's input and it completely keyboard accessible.

---
+++ js/access.js
@@ -0,0 +1,137 @@
+/*------------------------------------*\
+  ACCESSIBILITY AND MENUS - V3
+\*------------------------------------*/
+
+var timer;
+
+var dropdownMenu = ".nav-dropdown";
+var dropdownButton = ".nav-dropdown > a";
+var dropdownList = ".nav-dropdown > ul";
+var navButton = ".main-nav-btn";
+var navList = ".main-nav"
+var navIcon = "#main-nav-btn__icon"
+
+var hasTouch = false;
+
+window.addEventListener("touchstart", function onFirstTouch() {
+    console.log("Can touch!");
+	hasTouch = true;
+    window.removeEventListener("touchstart", onFirstTouch, false);
+}, false); 
+console.log(hasTouch);
+
+
+if (hasTouch == true) { // If touch, for each dropdown "a" link wait for touchstart
+	$(dropdownButton).each(function(i, el){
+		el.addEventListener("touchstart", function(event){
+				if ($(dropdownButton).attr("aria-expanded") == "true") {
+					closeMenu("dropdown");
+				} else {
+					openMenu("dropdown");
+				};
+		});
+	});
+};
+
+// If no touch, use hover and click
+$(dropdownMenu).each(function(i, el){
+	el.addEventListener("mouseover", dropdownMouseover);
+	el.addEventListener("mouseout", dropdownMouseout);
+	$(dropdownButton)[0].addEventListener("click", dropdownClick);
+	htmlClose("click", dropdownButton, "dropdown", dropdownMenu);
+});
+
+	function dropdownMouseover(event) {
+		openMenu("dropdown");
+		$(dropdownMenu).attr("hover", "true")
+		console.log("mouseover");
+	};
+
+	function dropdownMouseout(event) {
+		closeMenu("dropdownTimeout");
+		console.log("mouseout");
+	};
+
+	function dropdownClick(event) {
+		if ($(dropdownButton).attr("aria-expanded") == "true" && $(dropdownMenu).attr("hover") != "true") {
+			closeMenu("dropdown");
+			console.log("click close");
+		} else {
+			openMenu("dropdown");
+			$(dropdownMenu).attr("hover", "false");
+			console.log("click open");
+		};
+		event.preventDefault();
+		event.stopPropagation();
+	};
+
+
+if (hasTouch == true) { // If on mobile, use touch
+	el.addEventListener("touchstart", function(event){
+		if ($(navButton).attr("aria-expanded") == "true") {
+			closeMenu("nav");
+		} else {
+			openMenu("nav");
+		};
+			});
+};
+
+$(navButton).each(function(i, el){
+	el.addEventListener("click", navClick);
+	htmlClose("click", navButton, "nav", navButton);
+});
+
+
+function navClick(event){
+	if ($(navButton).attr("aria-expanded") == "true") {
+		closeMenu("nav");
+	} else {
+		openMenu("nav");
+	};
+}
+
+
+function htmlClose(clickType, ariaObj, closeObj, protectObj) {
+	$("html")[0].addEventListener(clickType, function(event){
+		if ($(ariaObj).attr("aria-expanded") == "true") { 
+			closeMenu(closeObj);
+		};
+	});
+
+	// When using menu, do not propagate interaction to HTML
+	$(protectObj)[0].addEventListener(clickType, function(event){
+		event.stopPropagation();
+	});
+};
+
+
+
+function openMenu(type) {
+	if (type == "nav") {
+		$(navButton).attr("aria-expanded", "true");
+		$(navList).slideDown("fast");
+		$(navButton).addClass("open");
+		$(navIcon).attr("class", "open");
+	} else if (type == "dropdown") {
+		$(dropdownList).slideDown("fast"); // Hard open
+		$(dropdownButton).attr("aria-expanded", "true");
+		clearTimeout(timer);
+	};
+};
+
+function closeMenu(type) {
+	if (type == "nav") {
+		$(navList).slideUp("fast");
+		$(navButton).attr("aria-expanded", "false");
+		$(navButton).removeClass("open");
+		$(navIcon).attr("class", "");
+	} else if (type == "dropdown") {
+		$(dropdownList).slideUp("fast"); // Hard close
+		$(dropdownButton).attr("aria-expanded", "false");
+	} else if (type == "dropdownTimeout") {
+		timer = setTimeout(function(event){
+				$(dropdownList).slideUp("fast");
+				$(dropdownButton).attr("aria-expanded", "false");
+		}, 1000);
+	}
+};
